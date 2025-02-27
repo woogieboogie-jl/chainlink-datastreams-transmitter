d12822305252788c3844139a10e7c9b92342b3e5
Milen Radkov <milenradkov@me.com>
2025-02-27 18:50:17 +0200

    fix: allow start without config file

--- server/config/config.ts
+++ server/config/config.ts
@@ -3,8 +3,13 @@ import path from 'path';
 import { load } from 'js-yaml';
 import { Config } from '../types';
 
-const __dirname = import.meta.dirname;
+const CONFIG_PATH = path.resolve(import.meta.dirname, '../../config.yml');
 
-export const config = load(
-  readFileSync(path.resolve(__dirname, '../../config.yml'), 'utf8')
-) as Config;
+export const config: Config = (() => {
+  try {
+    return load(readFileSync(CONFIG_PATH, 'utf8')) as Config;
+  } catch {
+    console.log('No config file provided - proceeding with empty config.');
+    return {} as Config;
+  }
+})();
