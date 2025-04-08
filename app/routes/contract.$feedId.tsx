a67bbfe8d841fa7f9fdd106defcfc72c811ae6e3
Milen Radkov <milenradkov@me.com>
2025-04-08 06:21:52 +0300

    fix: typo

--- app/routes/contract.$feedId.tsx
+++ app/routes/contract.$feedId.tsx
@@ -407,7 +407,7 @@ export default function ContractEVM() {
         <CardHeader>
           <CardTitle>Function arguments</CardTitle>
           <div className="text-sm text-muted-foreground pt-2">
-            Enter report arguments field names in the sequense the contract
+            Enter report arguments field names in the sequence the contract
             expects them to be passed in the selected method.
             <br />
             Valid arguments:
