6a6e95ea08f13b153d0bf155353d28d84445e1fd
woogieboogie <woogieboogie.jl@gmail.com>
2025-08-12 02:26:02 +0900

    chore: update sample contracts with up-to-date features and audit fixes

--- contracts/interfaces/IDataStreamsFeed.sol
+++ contracts/interfaces/IDataStreamsFeed.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IAdrastiaVerifierProxy} from "./IAdrastiaVerifierProxy.sol";
+import {IAdrastiaVerifierProxy} from "src/interfaces/IAdrastiaVerifierProxy.sol";
 
 /**
  * @title IDataStreamsFeed
